c48d3a9aef1900660321455fdfa16ba6d6b3f11a
Mayank Sharma <makedy07feb@gmail.com>
2026-03-19 18:18:33 +0530

    new project

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/serverless';
+
+export default defineConfig({
+  output: 'server', // SSR enabled
+  adapter: vercel(), // ✅ REQUIRED for Vercel
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
